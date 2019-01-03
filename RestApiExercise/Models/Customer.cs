ea487febbf7bc80ff89edd1405d50f2ea55d1823
Tom Eldar <tomeldar@gmail.com>
2019-01-02 21:01:18 -0700

    Initial Exercise - Service only

---
+++ RestApiExercise/Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestApiExercise.Models
+{
+    public class Customer
+    {
+        public ObjectId _id { get; set; }
+        [BsonElement("FirstName")]
+        public string FirstName { get; set; }
+        [BsonElement("LastName")]
+        public string LastName { get; set; }
+        [BsonElement("SSN")]
+        public string SSN { get; set; }
+    }
+}
---
+++ RestApiExercise/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RestApiExercise.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
